0d4dd85d926ab59cf25e519d5c5c8635c520a3e9
iara gerardi <iaragerardi0207@gmail.com>
2022-11-03 12:11:58 -0300

    ejercicio de servidor con express

--- servidorConExpress.js
+++ servidorConExpress.js
@@ -1,27 +1,6 @@
-// const express = require('express')
-// const app = express()
-// const port = process.env.PORT || 3000
-// const {fileProds} = require('./ManejoDeArchivos/ManejoDeArchivos')
-
-// const allProducts = fileProds.GetAll();
-// const randomID = Math.floor(Math.random() * (allProducts.length - 1) + 1);
-// const randomProduct = fileProds.GetByID(randomID);
-
-// app.listen(port, () => {
-//   console.log(`Example app listening on port ${port}`)
-// })
-
-// app.get('/productos', (req, res) => {
-//     return res.send(allProducts)
-// })
-
-// app.get('/productoRandom', (req, res) => {
-//   return res.json(randomProduct)
-// })
-
 const express = require('express')
 const app = express()
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 8080
 const fs = require('fs')
 
 class File {
