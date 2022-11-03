73673c326cce4d78d5965a612f82bed24ef27c97
iara gerardi <iaragerardi0207@gmail.com>
2022-11-03 18:28:52 -0300

    ejercicio servidor express

--- servidorConExpress.js
+++ servidorConExpress.js
@@ -1,23 +1,23 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 8080
-const {fileClass} = require('./ManejoDeArchivos/ManejoDeArchivos')
+const { fileClass } = require('./ManejoDeArchivos/ManejoDeArchivos')
 
 let randomProduct = null
 const fileProds = new fileClass(`${__dirname}\\ManejoDeArchivos\\productos.txt`);
 const allProducts = fileProds.GetAll();
 
-const randomNumber =()=>{
-const randomID = Math.floor(Math.random() * (allProducts.length - 1) + 1);
- randomProduct = fileProds.GetByID(randomID);
-} 
-      
+const randomNumber = () => {
+  const randomID = Math.floor(Math.random() * (allProducts.length - 1) + 1);
+  randomProduct = fileProds.GetByID(randomID);
+}
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
 
 app.get('/productos', (req, res) => {
-    return res.send(allProducts)
+  return res.send(allProducts)
 })
 
 app.get('/productoRandom', (req, res) => {
