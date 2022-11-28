52ed49ff6ae5f8ba2698aae3d8168c79a928ff27
iara gerardi <iaragerardi0207@gmail.com>
2022-11-28 20:38:21 -0300

    ejercicio 6

--- 6_WebSocket/public/main.js
+++ 6_WebSocket/public/main.js
@@ -1,25 +1,59 @@
 const socket = io()
 
-socket.on('messages', (data) => {
-  const html = data.map(msj => {
-    return `<div>
-  <strong>
-  ${msj.author}
-  </strong>
-  <em>${msj.text}</em>
-  </div>`
+function addProduct(data) {
+  const product = {
+    title: document.getElementById('title').value,
+    price: document.getElementById('price').value,
+    thumbnail: "http://localhost:8080/img/1669216236775-shampoo.jpg",
+  }
+
+  socket.emit('new-product', product)
+  return
+}
+
+socket.on('products', (data) => {
+  console.log(data)
+  const table = `<tr class="grid grid-cols-3 gap-x-12 gap-y-8">
+  <th>Titulo</th>
+  <th>Precio</th>
+  <th>Foto</th>
+</tr>`
+  const productList = data.map(product => {
+    return `
+    <tr class="mt-4 grid grid-cols-3 gap-x-12 gap-y-12 bg-red-100 p-3 justify-items-center items-center">
+      <td> ${product.title} </td>
+      <td> ${product.price} </td>
+      <td> <img class="w-12 h-12" src="${product.thumbnail}" alt="${product.title}"> </td>
+    </tr>`
   })
     .join(' ')
-
-  document.getElementById('messages').innerHTML = html
+console.log(productList)
+  document.getElementById('products').innerHTML = table + productList
 })
 
 function addMessage(data) {
+  console.log(data)
   const message = {
-    author: document.getElementById('username').value,
-    text: document.getElementById('text').value
+    email: document.getElementById('username').value,
+    text: document.getElementById('text').value,
+    date: Date.now()
   }
 
   socket.emit('new-message', message)
   return
 }
+
+socket.on('messages', (data) => {
+  const messagesList = data.map(msj => {
+    return `<div>
+    <div class="flex items-center">
+      <strong class="pr-2 text-lg">${msj.email}</strong>
+      <p class="font-extralight text-sm">${msj.date}</p>
+    </div>
+    <em>${msj.text}</em>
+    </div>`
+  })
+    .join(' ')
+
+  document.getElementById('messages').innerHTML = messagesList
+})
--- 6_WebSocket/server.js
+++ 6_WebSocket/server.js
@@ -1,32 +1,68 @@
-const express = require('express')
-const { Server: HttpServer } = require('http')
-const { Server: IOServer } = require('socket.io')
+const express = require('express');
+const { Server: HttpServer } = require('http');
+const { Server: IOServer } = require('socket.io');
 
-const PORT = 8080
-const app = express()
+const PORT = 8081;
+const app = express();
+const fs = require("fs");
+const path = require('path');
+const handlebars = require("express-handlebars");
 
-const httpServer = HttpServer(app)
-const io = new IOServer(httpServer)
+const httpServer = HttpServer(app);
+const io = new IOServer(httpServer);
 
-const mensajes = [
-  { author: 'Dario', text: 'los temas están separados en tres bloques' },
-  { author: 'Ivan', text: 'ivan' },
-  { author: 'Ariel', text: 'Tony, Bruce' },
-  { author: 'Pedro', text: 'Choco' }
-]
+const classPath = path.join(__dirname, '../' + '/2_ManejoDeArchivos/ManejoDeArchivos.js');
+const { fileClass } = require(classPath);
+
+const mensajesPath = path.join(__dirname, '/data/mensajes.json');
+const productosPath = path.join(__dirname, '/data/productos.json');
+
+const mensajesClass = new fileClass(mensajesPath);
+const productosClass = new fileClass(productosPath);
 
 app.use(express.static('./public'));
 
-io.on('connection', socket => {
-  console.log(mensajes.reverse())
+app.engine(
+  "handlebars",
+  handlebars.engine({
+    layoutsDir: __dirname + "/public/layouts/",
+  })
+);
 
-  socket.emit('messages', mensajes.reverse());
+app.set("view engine", "handlebars");
+app.set("views", __dirname + "/public");
+
+app.get("/products", async (req, res) => {
+  try {
+    res.render("desafio", {
+      layout: "index",
+    });
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+io.on('connection', socket => {
+
+  const mensajes = mensajesClass.GetAll();
+  socket.emit('messages', mensajes);
+  const productos = productosClass.GetAll();
+  socket.emit('products', productos);
 
   socket.on('new-message', (data) => {
-    mensajes.push(data)
+    console.log(data)
+    mensajesClass.save(data);
 
-    io.sockets.emit('messages', mensajes.reverse())
+    const mensajes = mensajesClass.GetAll();
+    io.sockets.emit('messages', mensajes)
+  })
+
+  socket.on('new-product', (data) => {
+    productosClass.save(data);
+
+    const productos = productosClass.GetAll();
+    io.sockets.emit('products', productos);
   })
 })
 
-httpServer.listen(PORT)
+httpServer.listen(PORT) 
