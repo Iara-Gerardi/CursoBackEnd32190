aee5777b653c52d8d9039536eb2fc0f244c000f1
iara gerardi <iaragerardi0207@gmail.com>
2022-12-05 11:42:40 -0300

    avances controller productos

--- 7_PrimeraEntregaTP/controllers/ProductsController.js
+++ 7_PrimeraEntregaTP/controllers/ProductsController.js
@@ -1,23 +1,77 @@
-const getProduct = async (req, res) => {
+const fs = require('fs');
+const path = require('path');
+const express = require('express');
 
+const admin = true;
+
+const jsonPath = path.join(__dirname, '../' + '/data/data.json');
+
+const { fileClass } = require('../../2_ManejoDeArchivos/ManejoDeArchivos');
+const fileProds = new fileClass(jsonPath);
+
+const getProduct = async (req, res) => {
+  try {
+    const allProducts = fileProds.GetAll();
+    return res.json({ message: 'success', data: allProducts });
+  } catch (err) {
+    return res.status(500).json({ error: err })
+  }
 }
 
 const getProductById = async (req, res) => {
-
+  const id = parseInt(req.params.id);
+  try {
+    const product = fileProds.GetByID(id);
+    if (product === null) return res.status(400).json({ message: 'error producto no encontrado' })
+    return res.json({ message: 'success', data: product })
+  } catch (err) {
+    return res.status(500).json({ error: err })
+  }
 }
-
 
 // Abajo, solo para admins
 const editProduct = async (req, res) => {
+  if (!admin) return res.status(400).json({ error: 'method availeable only for admins' });
+  const { id } = req.params
+  const { timestamp, nombre, descripcion, codigo, foto, precio, stock } = req.body
 
+  try {
+    if (!id || !timestamp || !nombre || !descripcion || !codigo || !foto || !precio || !stock) return res.status(400).json({ error: "all fields required" });
+    const editedObject = { id, title, price };
+    const product = fileProds.EditByID(editedObject);
+    return res.json({ data: product });
+  } catch (err) {
+    return res.status(500).json({ error: err });
+  }
 }
 
 const addProduct = async (req, res) => {
+  if (!admin) return res.status(400).json({ error: 'method availeable only for admins' });
 
+  const { timestamp, nombre, descripcion, codigo, foto, precio, stock } = req.body
+
+  if (!timestamp || !nombre || !descripcion || !codigo || !foto || !precio || !stock) return res.status(400).json({ error: "all fields required" });
+
+  try {
+    const newProd = { title, price, thumbnail };
+    const product = fileProds.save(newProd);
+    console.log(product)
+    return res.json({ message: 'success', data: product });
+  } catch (err) {
+    return res.status(500).json({ error: err });
+  }
 }
 
 const deleteProduct = async (req, res) => {
-
+  if (!admin) return res.status(400).json({ error: 'method availeable only for admins' });
+  const id = parseInt(req.params.id);
+  try {
+    const product = fileProds.DeleteByID(id);
+    if (product === null) return res.status(400).json({ message: 'error producto no encontrado' })
+    return res.json({ message: 'success', data: { productID: product } })
+  } catch (err) {
+    return res.status(500).json({ error: err })
+  }
 }
 
 module.exports = { getProduct, getProductById, editProduct, addProduct, deleteProduct }
